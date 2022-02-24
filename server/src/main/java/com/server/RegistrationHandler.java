0c867056c64f6f5e4cb0ac4ca4f14534a347ea0f
juvaa <julius.valimaa@gmail.com>
2022-02-24 23:13:59 +0200

    Change userauth to use json

--- server/src/main/java/com/server/RegistrationHandler.java
+++ server/src/main/java/com/server/RegistrationHandler.java
@@ -2,11 +2,15 @@ package com.server;
 
 import com.sun.net.httpserver.*;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.http.HttpHeaders;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
@@ -20,26 +24,69 @@ public class RegistrationHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange t) throws IOException {
         if (t.getRequestMethod().equalsIgnoreCase("POST")) {
-            InputStream stream = t.getRequestBody();
-            
-            String text = new BufferedReader(new InputStreamReader(stream,
-            StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
-            
-            String [] userInfo = text.split(":");
-            if (userInfo.length != 2) {
-                stream.close();
-                t.sendResponseHeaders(400, -1);
-            } else if (!(authenticator.addUser(userInfo[0], userInfo[1]))) {
-                String reponse = "User already registered\n";
+            Headers headers = t.getRequestHeaders();
+            String contentType;
+            if (headers.containsKey("Content-Type")) {
+                contentType = headers.get("Content-Type").get(0);
 
-                byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
-                t.sendResponseHeaders(403, bytes.length);
-                OutputStream messageBodyStream = t.getResponseBody();
-                messageBodyStream.write(bytes);
-                messageBodyStream.close();
+                if (contentType.equals("application/json")) {
+                    InputStream stream = t.getRequestBody();
+                
+                    String jsonString = new BufferedReader(new InputStreamReader(stream,
+                    StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+    
+                    try {
+                        JSONObject userJSON = new JSONObject(jsonString);
+                        String username = userJSON.getString("username");
+                        String password = userJSON.getString("password");
+                        String email = userJSON.getString("email");
+    
+                        if (username.length() == 0 || password.length() == 0 || email.length() == 0) {
+                            String reponse = "User info not proper\n";
+    
+                            byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
+                            t.sendResponseHeaders(400, bytes.length);
+                            OutputStream messageBodyStream = t.getResponseBody();
+                            messageBodyStream.write(bytes);
+                            messageBodyStream.close();
+                        }
+                        else if (!(authenticator.addUser(username, password, email))) {
+                            String reponse = "User already registered\n";
+                        
+                            byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
+                            t.sendResponseHeaders(403, bytes.length);
+                            OutputStream messageBodyStream = t.getResponseBody();
+                            messageBodyStream.write(bytes);
+                            messageBodyStream.close();
+                        } else {
+                            stream.close();
+                            t.sendResponseHeaders(200, -1);
+                        }
+                    } catch (JSONException e) {
+                        String reponse = "Error when parsing JSON object\n";
+                        byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
+                        t.sendResponseHeaders(400, bytes.length);
+                        OutputStream messageBodyStream = t.getResponseBody();
+                        messageBodyStream.write(bytes);
+                        messageBodyStream.close();
+                    }
+                } else {
+                    String reponse = "Wrong content type. Only JSON is supported\n";
+    
+                    byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
+                    t.sendResponseHeaders(400, bytes.length);
+                    OutputStream messageBodyStream = t.getResponseBody();
+                    messageBodyStream.write(bytes);
+                    messageBodyStream.close();
+                } 
             } else {
-                stream.close();
-                t.sendResponseHeaders(200, -1);
+                String reponse = "No content type available\n";
+    
+                    byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
+                    t.sendResponseHeaders(400, bytes.length);
+                    OutputStream messageBodyStream = t.getResponseBody();
+                    messageBodyStream.write(bytes);
+                    messageBodyStream.close();
             }
             
         } else {
--- server/src/main/java/com/server/UserAuthenticator.java
+++ server/src/main/java/com/server/UserAuthenticator.java
@@ -1,32 +1,37 @@
 package com.server;
 
-import java.util.Hashtable;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.sun.net.httpserver.*;
 
 public class UserAuthenticator extends BasicAuthenticator{
-    private Map<String, String> users = null;
+    private List<User> users = null;
 
     public UserAuthenticator() {
         super("coordinates");
-        users = new Hashtable<>();
-
-        users.put("dummy", "passwd");
+        users = new ArrayList<>();
     }
 
+    @Override
     public boolean checkCredentials(String username, String password) {
         /** Gets user password if user exists and then verifies the password.
         Returns true for valid user/password, false otherwise */
-        return password.equals(users.get(username));
-    }
-
-    public boolean addUser(String username, String password) {
-            if (users.containsKey(username)) {
-                return false;
-            } else {
-                users.put(username, password);
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
                 return true;
             }
         }
+        return false;
+    }
+
+    public boolean addUser(String username, String password, String email) {
+        for (User user: users) {
+            if (user.getUsername().equals(username)) {
+                return false;
+            }
+        }
+        users.add(new User(username, password, email));
+        return true;
+    }
 }
