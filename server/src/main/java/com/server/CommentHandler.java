6c23f7142158f2b2315c0fc644fc61c2e5326011
juvaa <julius.valimaa@gmail.com>
2022-03-18 22:33:05 +0200

    Refactor comment handler

--- server/src/main/java/com/server/CommentHandler.java
+++ server/src/main/java/com/server/CommentHandler.java
@@ -20,80 +20,78 @@ import org.json.JSONObject;
 
 public class CommentHandler implements HttpHandler {
     
-    /* TODO: Refactor this whole method */
     @Override
     public void handle(HttpExchange t) throws IOException {
-        CoordinateDatabase db = CoordinateDatabase.getInstance();
-
         if (t.getRequestMethod().equalsIgnoreCase("POST")) {
-            Headers headers = t.getRequestHeaders();
-            String contentType;
-            if (headers.containsKey("Content-Type")) {
-                contentType = headers.get("Content-Type").get(0);
+            handlePost(t);
+        } else {
+            String response = "Not supported\n";
 
-                if (contentType.equals("application/json")) {
-                    InputStream stream = t.getRequestBody();
+            sendResponse(response, 400, t);
+        }
+    }
+
+   /* TODO: Maybe refactor the if-else try-catch spaghetti */
+    private void handlePost(HttpExchange t) throws IOException {
+        CoordinateDatabase db = CoordinateDatabase.getInstance();
+        Headers headers = t.getRequestHeaders();
+        String contentType;
+        if (headers.containsKey("Content-Type")) {
+            contentType = headers.get("Content-Type").get(0);
+
+            if (contentType.equals("application/json")) {
+                InputStream stream = t.getRequestBody();
+                
+                String jsonString = new BufferedReader(new InputStreamReader(stream,
+                    StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+
+                stream.close();
+
+                try {
+                    JSONObject commentJSON = new JSONObject(jsonString);
+
+                    int coordinateId = commentJSON.getInt("id");
+                    String comment = commentJSON.getString("comment");
+                    String timestampString = commentJSON.getString("sent");
                     
-                    String jsonString = new BufferedReader(new InputStreamReader(stream,
-                        StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+                    if (coordinateId <= 0 || comment.length() == 0 ||
+                            timestampString.length() == 0) {
+                        String response = "Coordinate info not proper\n";
 
-                    try {
-                        JSONObject commentJSON = new JSONObject(jsonString);
-
-                        int coordinateId = commentJSON.getInt("id");
-                        String comment = commentJSON.getString("comment");
-                        String timestampString = commentJSON.getString("sent");
+                        sendResponse(response, 400, t);
+                    } else {
+                        db.setComment(
+                            new CoordinateComment(coordinateId, comment, timestampString));
                         
-                        if (coordinateId <= 0 || comment.length() == 0 ||
-                                timestampString.length() == 0) {
-                            String reponse = "Coordinate info not proper\n";
-    
-                            byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
-                            t.sendResponseHeaders(400, bytes.length);
-                            OutputStream messageBodyStream = t.getResponseBody();
-                            messageBodyStream.write(bytes);
-                            messageBodyStream.close();
-                        } else {
-                            db.setComment(
-                                new CoordinateComment(coordinateId, comment, timestampString));
-                            
-                            stream.close();
-                            t.sendResponseHeaders(200, -1);
-                        }
-                    } catch (JSONException | DateTimeParseException | SQLException e) {
-                        String reponse = "Error when parsing JSON object\n";
-                        byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
-                        t.sendResponseHeaders(400, bytes.length);
-                        OutputStream messageBodyStream = t.getResponseBody();
-                        messageBodyStream.write(bytes);
-                        messageBodyStream.close();
+                        sendResponse("", 200, t);
                     }
-                } else {
-                    String reponse = "Wrong content type. Only JSON is supported\n";
-    
-                    byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
-                    t.sendResponseHeaders(400, bytes.length);
-                    OutputStream messageBodyStream = t.getResponseBody();
-                    messageBodyStream.write(bytes);
-                    messageBodyStream.close();
+                } catch (JSONException | DateTimeParseException | SQLException e) {
+                    String response = "Error when parsing JSON object\n";
+
+                    sendResponse(response, 400, t);
                 }
             } else {
-                String reponse = "No content type available\n";
-    
-                byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
-                t.sendResponseHeaders(400, bytes.length);
-                OutputStream messageBodyStream = t.getResponseBody();
-                messageBodyStream.write(bytes);
-                messageBodyStream.close();
+                String response = "Wrong content type. Only JSON is supported\n";
+
+                sendResponse(response, 400, t);
             }
         } else {
-            String reponse = "Not supported\n";
+            String response = "No content type available\n";
 
-            byte [] bytes = reponse.getBytes(StandardCharsets.UTF_8);
-            t.sendResponseHeaders(400, bytes.length);
+            sendResponse(response, 400, t);
+        }
+    }
+
+    private void sendResponse(String response, int responseCode, HttpExchange t) throws IOException {
+        if (!response.isBlank()) {
+            byte [] bytes = response.getBytes(StandardCharsets.UTF_8);
+            t.sendResponseHeaders(responseCode, bytes.length);
             OutputStream messageBodyStream = t.getResponseBody();
             messageBodyStream.write(bytes);
             messageBodyStream.close();
+        } else {
+            t.sendResponseHeaders(responseCode, -1);
+            t.close();
         }
     }
 }
